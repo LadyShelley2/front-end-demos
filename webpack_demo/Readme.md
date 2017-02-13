c63ba3f2604cd494abb29bb9673455d8d4523aad
lbb <573328344@qq.com>
2017-02-13 10:27:41 +0800

    init

---
+++ webpack_demo/Readme.md
@@ -0,0 +1,3 @@
+A webpack demo
+
+https://zhuanlan.zhihu.com/p/20367175?columnSlug=FrontendMagazine
---
+++ webpack_demo/app/sub.js
@@ -0,0 +1,11 @@
+
+/**
+ * Created by lbb on 2017/2/13.
+ */
+function generateText() {
+    var element = document.createElement('h2');
+    element.innerHTML = "Hello h2 world";
+    return element;
+}
+
+module.exports = generateText;
